d6deae459a39620a013a6094a4987630fd626a08
Jessica Dior <JessicaClairDior@gmail.com>
2022-05-03 00:08:02 -0400

    commit tree f0234b24f3d299082b6947ca3ac424aac048e73c

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board, index)
-    if board[index] == " " || board[index] == "" || board[index] == nil
-        false
-    else 
-        true
+    if board[index] == " " or board[index] == "" or board[index] == nil
+    	return false
     end
+	true
 end
+
